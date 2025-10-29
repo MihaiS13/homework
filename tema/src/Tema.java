9386886f13e2f8735b8a5e7ed4de12bbaf689675
MihaiS <mihaisalagean0@gmail.com>
2025-10-30 00:26:31 +0200

    first commit

---
+++ tema/src/Tema.java
@@ -0,0 +1,19 @@
+public class Tema {
+public static void main(String[] args){
+//Declaram 3 variabile de tip String
+    String termen1 = "Imi place ";
+        String termen2 = "sa mananc ";
+    String termen3 = "Inghetata.";
+    //Printam rezultatul
+    System.out.println(termen1+ termen2 + termen3);
+    final String CNP = "2123317889587";
+    double numar1 = 5234567891.0;
+    double numar2 = 1167416798.0;
+    System.out.println(numar1 + numar2);
+    System.out.println(numar1 - numar2);
+    System.out.println(numar1 * numar2);
+    System.out.println(numar1 / numar2);
+    //Daca vreau sa vad zecimalele, pot sa folosesc double
+
+}
+}
