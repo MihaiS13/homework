1b288931c00e5b38649cf29d5c94947e24e7ca6b
S.Mihai <calipsocalipso91@yahoo.com>
2025-10-30 11:22:54 +0200

    second commit

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+public static void main(String[] args) {
+   double numar1 = 10;
+    double numar2 = 3;
+    System.out.println( numar1 + numar2);
+    System.out.println( numar1 - numar2);
+    System.out.println( numar1 * numar2);
+    System.out.println( numar1 / numar2);
+
+}
+}
