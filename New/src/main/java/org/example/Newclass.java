d49d62d7b98f9fed97910d05720a6990a7372564
S.Mihai <calipsocalipso91@yahoo.com>
2025-10-30 23:10:00 +0200

    test

---
+++ New/src/main/java/org/example/Newclass.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Newclass {
+private String firstName;
+    private String lastName;
+
+    public Person(String firstName , String LastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
