d34fe80cf6d795084bf1abdaed845ec3f6f77353
MihaiS13 <mihaisalagean0@gmail.com>
2025-10-30 22:50:00 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    static void main() {
+//        String primaVariabilaDeTipString;
+//        primaVariabilaDeTipString= "Hello";
+//
+//        String aDouaVariabilaDeTipString;
+//        aDouaVariabilaDeTipString="World";
+//
+//        String caracterSpatiu;
+//        caracterSpatiu=" ";
+//        String primulMeuSalut;
+//        primulMeuSalut= primaVariabilaDeTipString + caracterSpatiu + aDouaVariabilaDeTipString;
+//
+//        System.out.println(primulMeuSalut);
+
+//  int myFirstNo = 5;
+//          int mySecondNo= 9;
+//                  int sumOfNumbers = myFirstNo + mySecondNo;
+//
+//                  System.out.println(sumOfNumbers);
+
+//// Constants
+//        final int CNP = 187453625;
+//
+//        System.out.println(CNP);
+//
+//
+//        final String IBAN = "INGBRO26153612001"
+
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        int prod = a * b;
+        System.out.println(a + "+" + b + "=" + sum);
+        System.out.println(a + "+" + b + "=" + prod);
+
+
+    }
+}
+
+
+
+
